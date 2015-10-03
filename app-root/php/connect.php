36c5e8afc0fd07b371c8b5713504d9157a72d8e3
Leon <leonvojtech94@gmail.com>
2015-10-04 01:32:17 +0200

    zmena connectu

--- app-root/php/connect.php
+++ app-root/php/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-define('hostname', 'OPENSHIFT_MYSQL_DB_HOST');
-define('user', 'OPENSHIFT_MYSQL_DB_USERNAME');
-define('password', 'OPENSHIFT_MYSQL_DB_PASSWORD');
-define('databaseName', 'gameofflags');
+define('hostname', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('user', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('password', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('databaseName', getenv('OPENSHIFT_GEAR_NAME'));
 
 $connect = mysqli_connect(hostname, user, password, databaseName);          
 
