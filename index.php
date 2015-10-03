b3a98413ed302baa67fd6316c2eaa3bacebce198
Leon <leonvojtech94@gmail.com>
2015-10-04 00:56:04 +0200

    pridani php souboru

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1;url=./php">
   <title>Welcome to OpenShift</title>
 <body>
 Hello!
---
+++ php/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+define('hostname', 'https://gameofflags-vojtele1.rhcloud.com');
+define('user', 'admini7llZ7c');
+define('password', '4QAjFVA_J_uf');
+define('databaseName', 'gameofflags');
+
+$connect = mysqli_connect(hostname, user, password, databaseName);          
+
+?>
+
