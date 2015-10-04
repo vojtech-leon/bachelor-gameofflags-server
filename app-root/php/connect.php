fa63d518d31290193c3e4d35679be84ba91bcf0b
Leon <leonvojtech94@gmail.com>
2015-10-04 14:30:27 +0200

    zmena connect

--- app-root/php/connect.php
+++ app-root/php/connect.php
@@ -20,7 +20,7 @@ if (!$connect) {
     exit;
 }
 
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Success: A proper connection to MySQL was made! The " . $databaseName . " database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($connect) . PHP_EOL;
 
 mysqli_close($connect);      
