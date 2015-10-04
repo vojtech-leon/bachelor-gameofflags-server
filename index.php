86f417ce42aa84a81a0594c64685248c17d76d6b
Leon <leonvojtech94@gmail.com>
2015-10-04 15:39:29 +0200

    smazani souboru

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1;url=./php">
-  <title>Welcome to OpenShift</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1;url=./app-root">
+  <title>Welcome to my project</title>
 <body>
 Hello!
+Tady asi časem něco dodám.
 
 </body>
 </html>
