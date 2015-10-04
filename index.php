3aef1a21335331cdde2cf714739fa39a9f7560bd
Leon <leonvojtech94@gmail.com>
2015-10-04 16:58:56 +0200

    pridani dibi

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<!--
+<?php 
+include 'dibi.min.php';
+?>
 <!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1;url=./app-root">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1;">
   <title>Welcome to my project</title>
 <body>
 Hello!
@@ -11,5 +13,4 @@ Tady asi časem něco dodám.
 
 </body>
 </html>
--->
-<meta http-equiv="refresh" content="0;url=./app-root">
+
