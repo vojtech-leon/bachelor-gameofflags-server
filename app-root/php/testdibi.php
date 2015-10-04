56323eca493796bc4aafb69a1825233c259b8e1b
Leon <leonvojtech94@gmail.com>
2015-10-04 17:46:36 +0200

    smazani souboru

--- app-root/php/testdibi.php
+++ app-root/php/testdibi.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 dibi::connect(array(
     'driver'   => 'mysql',
     'host'     => getenv('OPENSHIFT_MYSQL_DB_HOST'),
@@ -11,19 +11,33 @@ dibi::connect(array(
 
 
 
-$result = dibi::query('SELECT * FROM student WHERE age < 500');
+$vysledek = dibi::query('SELECT * FROM student WHERE age < 500');
 
-
+/*
 foreach ($result as $n => $row) {
     print_r($row);
 }
 
 unset($result);
-
 */
+
 ?>
 
 <?php 
 echo "Ahoj";
 
+?>
+
+<table>
+<?php
+//$vysledek=mysql_query("select * from student where age < 500");
+  while ($zaznam=MySQL_Fetch_Array($vysledek)):
+    ?>
+    <TR>
+      <TD><?echo $zaznam["firstname"]?></TD>
+      <TD><?echo $zaznam["lastname"]?></TD>    
+	  <TD><?echo $zaznam["age"]?></TD>    
+    </TR>
+    <?    
+  endwhile;
 ?>
