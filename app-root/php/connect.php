7caaf9fc624daef1e0f252017e33ed519c262701
Leon <leonvojtech94@gmail.com>
2015-10-04 02:19:39 +0200

    pridani php souboru

--- app-root/php/connect.php
+++ app-root/php/connect.php
@@ -1,11 +1,17 @@
 <?php
 
-define('hostname', getenv('OPENSHIFT_MYSQL_DB_HOST' || 'localhost'));
+/*define('hostname', getenv('OPENSHIFT_MYSQL_DB_HOST' || 'localhost'));
 define('user', getenv('OPENSHIFT_MYSQL_DB_USERNAME' || 'admini7llZ7c'));
 define('password', getenv('OPENSHIFT_MYSQL_DB_PASSWORD' || '4QAjFVA_J_uf'));
 define('databaseName', getenv('OPENSHIFT_GEAR_NAME' || 'gameofflags'));
+*/
+$hostname = getenv('OPENSHIFT_MYSQL_DB_HOST') || 'localhost';
+$user = getenv('OPENSHIFT_MYSQL_DB_USERNAME') || 'admini7llZ7c';
+$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD' || '4QAjFVA_J_uf';
+$databaseName = getenv('OPENSHIFT_GEAR_NAME' || 'gameofflags';
 
-$connect = mysqli_connect(hostname, user, password, databaseName);    
+// $connect = mysqli_connect(hostname, user, password, databaseName);    
+$connect = mysqli_connect($hostname, $user, $password, $databaseName);  
 
 if (!$connect) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
