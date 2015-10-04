e618b2897e711bcd1039d22121168ba13c48fdb1
Leon <leonvojtech94@gmail.com>
2015-10-04 15:17:32 +0200

    zmena connect

--- app-root/php/showStudents.php
+++ app-root/php/showStudents.php
@@ -7,7 +7,7 @@ function showStudent()
 {
 	global $connect;
 	
-	$query = " Select * FROM STUDENT; ";
+	$query = " select * from student; ";
 	
 	$result = mysqli_query($connect, $query);
 	$number_of_rows = mysqli_num_rows($result);
