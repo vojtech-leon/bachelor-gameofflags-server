f5b0537af0b76d1b9ac5e285ad73a8fbbfa6a5f7
Leon <leonvojtech94@gmail.com>
2015-10-04 17:19:09 +0200

    zkouska dibi

---
+++ app-root/php/testdibi.php
@@ -0,0 +1,24 @@
+<?php
+
+dibi::connect(array(
+    'driver'   => 'mysql',
+    'host'     => getenv('OPENSHIFT_MYSQL_DB_HOST'),
+    'username' => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+    'password' => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+    'database' => getenv('OPENSHIFT_GEAR_NAME'),
+    'charset'  => 'utf8',
+));
+
+
+
+$result = dibi::query('SELECT * FROM student WHERE age < 500');
+
+
+foreach ($result as $n => $row) {
+    print_r($row);
+}
+
+unset($result);
+
+echo "Ahoj";
+?>
