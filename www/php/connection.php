a9188b11b8ea9fd7a5deb815e69660a491f62b8c
vojtele1 <leonvojtech94@gmail.com>
2015-12-03 20:59:57 +0100

    promazani gitu

--- www/php/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('hostname', 'localhost');
-define('user', 'root');
-define('password', '');
-define('databaseName', 'gameofflags');
-$connect = mysqli_connect(hostname, user, password, databaseName);
-?>
